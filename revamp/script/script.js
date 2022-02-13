c69f4b4ae4722fb4dd357a31c82cfc168ca3bc66
Mike <freestyle.431@hotmail.com>
2022-02-13 21:29:30 +0100

    site revamp - mobile version done

--- revamp/script/script.js
+++ revamp/script/script.js
@@ -10,14 +10,33 @@ $(function($){
     })
 })
 
-$('.buttons button').on('click', function() {
-    const goToSection = '[data-section=' + $(this).attr('class') + ']';
+$('.desktop-btn, .mobile-btn').on('click', function () {
+    const $goToSection = '[data-section=' + $(this).attr('goto-section') + ']';
+    // console.log($goToSection)
+    // console.log($($goToSection).offset().top)
     $('body, html').animate({
-        scrollTop: $(goToSection).offset().top
+        scrollTop: $($goToSection).offset().top
     })
 })
 
-$('.hamburger, .buttons button').on('click', function () {
-    $('.fa-bars, .fa-arrow-right, .mobile').toggleClass('active')
+$('.hamburger, .mobile-btn').on('click', function () {
+    $('.fa-bars, .fa-arrow-right, .nav-mobile').toggleClass('active')
 })
 
+$(function($) {
+    const $arrow = $('.fa-arrow-circle-up');
+    $(window).scroll(function(event) {
+        const $current = $(this).scrollTop();
+        if($current > 0) {
+            $arrow.addClass('active');
+        } else {
+            $arrow.removeClass('active');
+        }
+    })
+})
+
+$('.nav-mobile__up-top-arrow').on('click', () => {
+    $('body, html').animate({
+        scrollTop: '0px'
+    })
+})
