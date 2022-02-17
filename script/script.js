29d2d61db960fd86939d78223da26034487e4ac9
Mike <freestyle.431@hotmail.com>
2022-02-17 18:39:56 +0100

    revamped files moved to main folder. Old files deleted; new 'offer' section image added.

---
+++ script/script.js
@@ -0,0 +1,42 @@
+$(function($){
+    var $navbar = $('.nav-desktop');
+    $(window).scroll(function(event){
+        var $current = $(this).scrollTop();
+        if( $current > 0 ){
+            $navbar.addClass('nav-desktop--dark');
+        } else{
+            $navbar.removeClass('nav-desktop--dark');
+        }
+    })
+})
+
+$('.desktop-btn, .mobile-btn').on('click', function () {
+    const $goToSection = '[data-section=' + $(this).attr('goto-section') + ']';
+    // console.log($goToSection)
+    // console.log($($goToSection).offset().top)
+    $('body, html').animate({
+        scrollTop: $($goToSection).offset().top
+    })
+})
+
+$('.hamburger, .mobile-btn').on('click', function () {
+    $('.fa-bars, .fa-arrow-right, .nav-mobile').toggleClass('active')
+})
+
+$(function($) {
+    const $arrow = $('.fa-arrow-circle-up');
+    $(window).scroll(function(event) {
+        const $current = $(this).scrollTop();
+        if($current > 0) {
+            $arrow.addClass('active');
+        } else {
+            $arrow.removeClass('active');
+        }
+    })
+})
+
+$('.nav-mobile__up-top-arrow').on('click', () => {
+    $('body, html').animate({
+        scrollTop: '0px'
+    })
+})
